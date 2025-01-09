ade79697e6c35c204c65906cdc978bdd2d8fe585
Willi Kappler <willi.kappler.gm@gmail.com>
2025-01-09 21:35:36 +0100

    Finish camera and qr code scan.

--- src/sample_data_app/main.py
+++ src/sample_data_app/main.py
@@ -1,30 +1,52 @@
-from kivy.app import App
-#from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.anchorlayout import AnchorLayout
 
-import plyer
+import numpy as np
+import cv2
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
+
 
 class SDApp(App):
+    cam = Camera(resolution=(320, 240))
+
     def build(self):
-        parent = AnchorLayout(anchor_x="center", anchor_y="top")
+        #parent = AnchorLayout(anchor_x="center", anchor_y="top")
+        parent = BoxLayout(orientation="vertical")
+
+        self.cam.play = True
 
         scan_button = Button(text="Scan QR code")
         scan_button.bind(on_release=self.scan_image)
 
+        parent.add_widget(self.cam)
         parent.add_widget(scan_button)
 
         return parent
 
     def scan_image(self, obj):
         print("Scan image...")
-        try:
-            plyer.camera.take_picture("qr_code", self.scan_qr_code)
-        except NotImplementedError:
-            print("Camera not available...")
 
-    def scan_qr_code(self):
-        pass
+        # Get texture, size and pixel data
+        texture = self.cam.texture
+        height, width = texture.height, texture.width
+        pixels = texture.pixels
+
+        # Converto to image
+        data = np.frombuffer(pixels, np.uint8)
+        image = data.reshape(height, width, 4)
+
+        # QR code detector
+        detector = cv2.QRCodeDetector()
+        data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+
+        if vertices_array is not None:
+            print(f"QRCode data: {data}")
+            self.cam.play = False
+        else:
+            print("There was some error")
+
 
 if __name__ == "__main__":
     sd_app = SDApp()
