a0dc13ea701896240213e1cf787a077d50868700
Willi Kappler <willi.kappler.gm@gmail.com>
2025-01-20 22:29:35 +0100

    Add support for GPS

--- src/sample_data_app/sd_edit_data.py
+++ src/sample_data_app/sd_edit_data.py
@@ -6,6 +6,9 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
+from kivy.utils import platform
+
+from plyer import gps
 
 class SDEditScreen(Screen):
     def __init__(self, main_app, name: str):
@@ -14,6 +17,18 @@ class SDEditScreen(Screen):
         self.main_app = main_app
 
         super(SDEditScreen, self).__init__(name=name)
+
+        # Try to setup GPS:
+        self.has_gps = False
+        try:
+            gps.configure(on_location=self.process_gps_location) # type: ignore
+
+            if platform == "android":
+                self.request_android_permissions()
+
+            self.has_gps = True
+        except NotImplementedError:
+            print("GPS is not available")
 
         layout = BoxLayout(orientation="vertical")
 
@@ -40,6 +55,16 @@ class SDEditScreen(Screen):
         row_layout.add_widget(label)
         row_layout.add_widget(self.gps_location)
         layout.add_widget(row_layout)
+
+        if self.has_gps:
+            button_start = Button(text="Start GPS")
+            button_start.bind(on_release=lambda obj: gps.start()) # type: ignore
+            button_stop = Button(text="Stop GPS")
+            button_stop.bind(on_release=lambda obj: gps.stop()) # type: ignore
+            row_layout = BoxLayout(orientation="horizontal")
+            row_layout.add_widget(button_start)
+            row_layout.add_widget(button_stop)
+            layout.add_widget(row_layout)
 
         # Fourth row
         button = Button(text="Select a photo")
@@ -116,4 +141,17 @@ class SDEditScreen(Screen):
         tmp_data["description"] = self.description.text
         tmp_data["gps_location"] = self.gps_location.text
         self.main_app.choose_file_screen()
+
+    def process_gps_location(self, **kwargs):
+        lat = kwargs["lat"]
+        lon = kwargs["lon"]
+
+        self.gps_location.text = f"{lat=}, {lon=}"
+
+    def request_android_permissions(self):
+        from android.permissions import request_permissions, Permission # type: ignore
+
+        request_permissions([Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION])
+
+
 
